20fdb317e57403d0570f854bd5df90d3c9e96004
abojii <162477681+abojii@users.noreply.github.com>
2024-09-17 00:11:27 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox lstConteoSum;
+        private System.Windows.Forms.Button btnDados;
+        private System.Windows.Forms.Label lblDado1;
+        private System.Windows.Forms.Label lblDado2;
+        private System.Windows.Forms.Label lblSuma;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstConteoSum = new System.Windows.Forms.ListBox();
+            this.btnDados = new System.Windows.Forms.Button();
+            this.lblDado1 = new System.Windows.Forms.Label();
+            this.lblDado2 = new System.Windows.Forms.Label();
+            this.lblSuma = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstConteoSum
+            // 
+            this.lstConteoSum.FormattingEnabled = true;
+            this.lstConteoSum.ItemHeight = 20;
+            this.lstConteoSum.Location = new System.Drawing.Point(12, 154);
+            this.lstConteoSum.Name = "lstConteoSum";
+            this.lstConteoSum.Size = new System.Drawing.Size(208, 164);
+            this.lstConteoSum.TabIndex = 1;
+            // 
+            // btnDados
+            // 
+            this.btnDados.Image = global::Ejercicio_1.Properties.Resources.dado;
+            this.btnDados.Location = new System.Drawing.Point(56, 100);
+            this.btnDados.Name = "btnDados";
+            this.btnDados.Size = new System.Drawing.Size(101, 35);
+            this.btnDados.TabIndex = 2;
+            this.btnDados.UseVisualStyleBackColor = true;
+            this.btnDados.Click += new System.EventHandler(this.btnLanzarDados_Click);
+            // 
+            // lblDado1
+            // 
+            this.lblDado1.AutoSize = true;
+            this.lblDado1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDado1.Location = new System.Drawing.Point(27, 35);
+            this.lblDado1.Name = "lblDado1";
+            this.lblDado1.Size = new System.Drawing.Size(79, 22);
+            this.lblDado1.TabIndex = 3;
+            this.lblDado1.Text = "Dado 1: ";
+            // 
+            // lblDado2
+            // 
+            this.lblDado2.AutoSize = true;
+            this.lblDado2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDado2.Location = new System.Drawing.Point(135, 37);
+            this.lblDado2.Name = "lblDado2";
+            this.lblDado2.Size = new System.Drawing.Size(79, 22);
+            this.lblDado2.TabIndex = 4;
+            this.lblDado2.Text = "Dado 2: ";
+            // 
+            // lblSuma
+            // 
+            this.lblSuma.AutoSize = true;
+            this.lblSuma.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSuma.Location = new System.Drawing.Point(78, 72);
+            this.lblSuma.Name = "lblSuma";
+            this.lblSuma.Size = new System.Drawing.Size(100, 22);
+            this.lblSuma.TabIndex = 5;
+            this.lblSuma.Text = "Resultado: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tirar";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(247, 360);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSuma);
+            this.Controls.Add(this.lblDado2);
+            this.Controls.Add(this.lblDado1);
+            this.Controls.Add(this.btnDados);
+            this.Controls.Add(this.lstConteoSum);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Simulador de Dados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
